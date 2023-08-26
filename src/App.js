991b85f8eaccec448cae2322cb2af42c3e724895
unknown <danielmomusic@gmail.com>
2023-08-26 17:47:43 -0500

    Little active link fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import AboutMe from "./pages/AboutMe/AboutMe";
 import Projects from "pages/Projects/Projects";
 import Contact from "pages/Contact/Contact";
 import video from "./assets/coding.mp4";
+import { useEffect } from "react";
 
 function App() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate("/");
+    }, []);
+
     return (
         <div className="App">
             <section className="App-content">
@@ -19,6 +26,8 @@ function App() {
                 <div style={{ zIndex: "100" }}>
                     <Routes>
                         <Route path="/" element={<AboutMe />} />
+
+                        <Route path="/portfolio" element={<AboutMe />} />
 
                         <Route path="/projects" element={<Projects />} />
 
